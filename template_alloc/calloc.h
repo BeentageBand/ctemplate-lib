faccb4b953986c73f549e7743d8d98fc506d1ca3
BeentageBand <josera.baez@gmail.com>
2017-12-03 13:02:02 -0600

    everything

---
+++ template_alloc/calloc.h
@@ -0,0 +1,49 @@
+#ifndef CALLOC_H_
+#define CALLOC_H_
+
+#include "ctemplate.h"
+
+#ifndef CAlloc_Params
+#error "CAlloc_Params not defined"
+#endif
+
+#define T_CAlloc(...) EVAL4(TEMPLATE(__VA_ARGS__))
+#define _T1 T_Param(1, CAlloc_Params)
+
+typedef union T_Calloc(Calloc_Params)
+{
+	union T_Calloc(Calloc_Params, Class) _private * _private vtbl;
+	struct
+	{
+		struct Object Object;
+		_T1 _private * _private buff;
+		size_t _private end;
+	};
+}T_Calloc(Calloc_Params, T);
+
+typedef T_Calloc(Calloc_Params, T) T_Calloc(Heap, Calloc_Params, T);
+typedef T_Calloc(Calloc_Params, T) T_Calloc(Pool, Calloc_Params, T);
+
+
+typedef union T_Calloc(Calloc_Params, Class)
+{
+	struct
+	{
+		struct Class Class;
+		_T1 * (* _private begin)(union T_Calloc(Calloc_Params) * const);
+		_T1 * (* _private end)(union T_Calloc(Calloc_Params) * const);
+		_T1 * (*_private alloc)(union T_Calloc(Calloc_Params) * const);
+		void (* _private free)(union T_Calloc(Calloc_Params) * const, _T1 * const);
+		size_t (* _private capacity)(union T_Calloc(Calloc_Params) * const);
+	};
+}T_Calloc(Calloc_Params, Class, T);
+
+extern union T_Calloc(Calloc_Params, Class) T_Calloc(Heap, Calloc_Params, Class);
+extern union T_Calloc(Calloc_Params, Class) T_Calloc(Pool, Calloc_Params, Class);
+
+extern void Method_Name(Populate, Heap, Calloc, Calloc_Params)(union T_Calloc(Calloc_Params) * const this, size_t const alloc_size);
+
+extern void Method_Name(Populate, Pool, Calloc, Calloc_Params)(union T_Calloc(Calloc_Params) * const this,
+		_T1 * const allocated_buff, size_t const alloc_size);
+
+#endif /*CALLOC_H_*/
