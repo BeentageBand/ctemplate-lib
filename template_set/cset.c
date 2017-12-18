41aa5dfb81b5fa3ab273f526fba9c68ff959c437
BeentageBand <josera.baez@gmail.com>
2017-12-18 00:01:52 -0600

    cset compiles

--- template_set/cset.c
+++ template_set/cset.c
@@ -6,32 +6,42 @@
 
 #define CSet_T TEMPLATE(CSet, CSet_Params)
 #define CSet_Class_T TEMPLATE(CSet, CSet_Params, Class)
-#define T1 CAT(T_Params(1, CSet_Params), _T)
+#define T1 T_Param(1, CSet_Params)
 #define CSet_Method(method) TEMPLATE(CSet, CSet_Params, method)
 
-static int CSet_Method(compare)(void *, void *);
-static void CSet_Method(delete)(union CSet_T * const);
-static uint32_t CSet_Method(size)(struct Object * const obj);
+static void CSet_Method(delete)(struct Object * const obj);
+static uint32_t CSet_Method(size)(union CSet_T * const);
 static void CSet_Method(clear)(union CSet_T * const);
-static t1 * CSet_Method(begin)(union CSet_T * const);
-static t1 * CSet_Method(end)(union CSet_T * const);
-static t1 * CSet_Method(at)(union CSet_T * const, uint32_t const);
-static t1 CSet_Method(access)(union CSet_T * const, uint32_t const);
-static void CSet_Method(insert)(union CSet_T * const, t1 const);
-static t1 * CSet_Method(find)(union CSet_T * const, t1 const * const);
-static t1 * CSet_Method(erase)(union CSet_T * const, t1 const * const);
+static T1 * CSet_Method(begin)(union CSet_T * const);
+static T1 * CSet_Method(end)(union CSet_T * const);
+static T1 * CSet_Method(at)(union CSet_T * const, uint32_t const);
+static T1 CSet_Method(access)(union CSet_T * const, uint32_t const);
+static void CSet_Method(insert)(union CSet_T * const, T1 const);
+static T1 * CSet_Method(find)(union CSet_T * const, T1 const);
+static void CSet_Method(erase)(union CSet_T * const, T1 const);
+static int CSet_Method(compare)(void const *, void const *);
 
-void CSet_Method(Init)(void)
+union CSet_Class_T CSet_Class_T =
 {
-	Member_Name(CSet_T,_Obj).capacity = 0;
-	Member_Name(CSet_T,_Obj).size = 0;
-	Member_Name(CSet_T,_Obj).buffer = NULL;
-	Member_Name(CSet_T,_Obj).cmp = CSet_Method(default_cmp);
-}
+	{{CSet_Method(delete), NULL},
+    CSet_Method(size),
+    CSet_Method(clear),
+    CSet_Method(begin),
+    CSet_Method(end),
+    CSet_Method(at),
+    CSet_Method(access),
+    CSet_Method(insert),
+    CSet_Method(find),
+    CSet_Method(erase),
+    CSet_Method(compare)
+	}
+};
+
+static union CSet_T CSet_T = {NULL};
 
 void CSet_Method(delete)(struct Object * const obj)
 {
-	union CSet_T * const this = Object_Cast(&CSet_Class_T.Class, obj);
+	union CSet_T * const this = (union CSet_T *) Object_Cast(&CSet_Class_T.Class, obj);
 	Isnt_Nullptr(this, );
 	this->vtbl->clear(this);
 }
@@ -41,52 +51,29 @@ int CSet_Method(compare)(void const * a, void const * b)
 	return memcmp(a,b, sizeof(T1));
 }
  
-void Method_Name(Populate, CSet, CSet_Params)(union CSet_T const * s)
+void Method_Name(Populate, CSet, CSet_Params)(union CSet_T * const this, T1 * const buff, size_t const buff_size)
 {
 	if(NULL == CSet_T.vtbl)
 	{
 		CSet_T.vtbl = &CSet_Class_T;
 		CSet_T.i = 0;
-		CSet_T.buff = NULL;
-    this->size = s->size;
-    this->capacity = s->capacity;
-    this->buffer = (T1*)malloc(sizeof(T1)*s->size);
-
-    memcpy(this->buffer, s->buffer, this->size);
-   return this;
+		CSet_T.buffer = NULL;
+	}
+	
+	memcpy(this, &CSet_T, sizeof(CSet_T));
+	
+    this->capacity = buff_size;
+    this->buffer = buff;
 }
 
-union CSet_T * CSet_Method(Set_New)(union CSet_T const * s)
-{
-    Constructor_New_Impl(CSet_T, Set, s)
-}
-
-union CSet_T CSet_Method(Cmp)( * const this, Member_Name(CSet_T,Cmp_T) const cmp)
-{
-
-   union CSet_T this = CSet_Method(Populate)();
-    this->cmp = cmp;
-   return this;
-}
-
-union CSet_T * CSet_Method(Cmp_New)( * const this, Member_Name(CSet_T,_Cmp_T) const cmp)
-{
-    Constructor_New_Impl(CSet_T, Cmp_New, cmp)
-}
-
-T1 * const CSet_Method(begin)(union CSet_T * const this)
+T1 * CSet_Method(begin)(union CSet_T * const this)
 {
     return this->buffer;
 }
 
-T1 * const CSet_Method(end)(union CSet_T * const this)
+T1 * CSet_Method(end)(union CSet_T * const this)
 {
     return this->buffer + this->vtbl->size(this);
-}
-
-uint32_t CSet_Method(capacity)(union CSet_T * const this)
-{
-    return this->capacity;
 }
 
 uint32_t CSet_Method(size)(union CSet_T * const this)
@@ -96,46 +83,56 @@ uint32_t CSet_Method(size)(union CSet_T * const this)
 
 void CSet_Method(clear)(union CSet_T * const this)
 {
-    this->capacity = 0;
-    this->i = 0;
-    this->buffer = NULL;
+	while(this->i)
+	{
+		T1 * const it = this->vtbl->begin(this);
+		this->vtbl->erase(this, *it);
+	}
 }
 
-void CSet_Method(insert)(union CSet_T * const this, T1 const * value)
+void CSet_Method(insert)(union CSet_T * const this, T1 const value)
 {
-    if (this->size >= this->capacity)
-    {
-        this->vtbl->reserve(this, this->capacity +5);
-    }
+    if (this->i >= this->capacity) return;
 
-    memcpy(&this->buffer[this->size++], value, sizeof(T1));
-    qsort(this->buffer, this->size, sizeof(T1), this->cmp);
+	T1 * end = this->vtbl->end(this);
+	T1 * found = this->vtbl->find(this, value);
+
+	if(end != found) return;
+
+    this->buffer[this->i++] = value;
+    qsort(this->buffer, this->i, sizeof(T1), this->vtbl->compare);
 }
 
 void CSet_Method(erase)(union CSet_T * const this,
-      T1 * const begin, T1 * const end)
+      T1 const value)
 {
-   for( T1 * it = begin; it != end; ++it)
-   {
-      _delete(it);
-   }
+    if (this->i >= this->capacity) return;
 
-   this->size = end - this->vtbl->begin(this);
-   memcpy(begin, end, this->size);
+	T1 * end = this->vtbl->end(this);
+	T1 * found = this->vtbl->find(this, value);
 
+	if(end == found) return;
+
+#ifdef CSET_DELETABLE
+	_delete(found);
+#endif
+
+	memset(found, 0, sizeof(found[0]));
+    memcpy(found - 1, found, (size_t) end - (size_t)found);
 }
 
-T1 * const CSet_Method(find)(union CSet_T * const this, T1 const * key)
+T1 * CSet_Method(find)(union CSet_T * const this, T1 const key)
 {
-   return (T1 * const) bsearch(key, this->buffer, this->size, sizeof(T1), this->cmp);
+   T1 * const found = bsearch(&key, this->buffer, this->i, sizeof(T1), this->vtbl->compare);
+   return (found)? found : this->vtbl->end(this);
 }
 
-uint32_t CSet_Method(count)(union CSet_T * const this, T1 const * key)
+T1 * CSet_Method(at)(union CSet_T * const this, uint32_t const index)
 {
-	return 0;
+    return this->buffer + index;
 }
 
-T1 CSet_Method(at)(union CSet_T * const this, uint32_t index)
+T1 CSet_Method(access)(union CSet_T * const this, uint32_t const index)
 {
     return this->buffer[index];
 }
--- template_set/cset.h
+++ template_set/cset.h
@@ -3,13 +3,13 @@
  
 #include "ctemplate.h"
 
-#ifdef CSet_Params
+#ifndef CSet_Params
 #error "CSet_Params not defined"
 #endif
 
 #define CSet_T TEMPLATE(CSet, CSet_Params)
 #define CSet_Class_T TEMPLATE(CSet, CSet_Params, Class)
-#define T1 CAT(T_Params(1, CSet_Params), _T)
+#define T1 T_Param(1, CSet_Params)
 
 #ifdef __cplusplus
 extern "C" {
@@ -21,35 +21,40 @@ typedef union CSet_T
 	struct
 	{
 		struct Object Object;
-		uint32_t _private size;
+		uint32_t _private i;
 		uint32_t _private capacity;
-		t1 _private * buffer;
-		int (* _private compare)(void * void *);
+		T1 _private * buffer;
 	};
-}TEMPLATE(CSet, CSet_Params, _T);
+}TEMPLATE(CSet, CSet_Params, T);
 
 typedef union CSet_Class_T
 {
 	struct
 	{
 		struct Class Class;
-		void (* _private clear)(union cset_t * const);
-		t1 * (* _private begin)(union cset_t * const);
-		t1 * (* _private end)(union cset_t * const);
-		t1 * (* _private at)(union cset_t * const, uint32_t const);
-		t1 (* _private access)(union cset_t * const, uint32_t const);
-		void (* _private insert)(union cset_t * const, t1 const);
-		t1 * (* _private find)(union cset_t * const, t1 const * const);
-		t1 * (* _private erase)(union cset_t * const, t1 const * const);
+		uint32_t (* _private size)(union CSet_T * const);
+		void (* _private clear)(union CSet_T * const);
+		T1 * (* _private begin)(union CSet_T * const);
+		T1 * (* _private end)(union CSet_T * const);
+		T1 * (* _private at)(union CSet_T * const, uint32_t const);
+		T1 (* _private access)(union CSet_T * const, uint32_t const);
+		void (* _private insert)(union CSet_T * const, T1 const);
+		T1 * (* _private find)(union CSet_T * const, T1 const);
+		void (* _private erase)(union CSet_T * const, T1 const);
+		int (* _private compare)(void const *, void const *);
 	};
 }TEMPLATE(CSet, CSet_Params, Class_T);
 
-extern CSet_Class_T _private CSet_Class_T;
+extern union CSet_Class_T _private CSet_Class_T;
 
-extern Method_Name(Populate, CSet, CSet_Params)(union CSet_T * const this, T1 * const buff,
+extern void Method_Name(Populate, CSet, CSet_Params)(union CSet_T * const set, T1 * const buff,
 		size_t const buff_size);
  
 #ifdef __cplusplus
 }
 #endif
+
+#undef CSet_T 
+#undef CSet_Class_T 
+#undef T1 
 #endif /*CSET_H_*/
