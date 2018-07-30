16c123aa58e793eefee3b49d510f5f836449f6da
BeentageBand <josera.baez@gmail.com>
2018-07-29 21:03:41 -0500

    add chash_map

---
+++ chash_map.c
@@ -0,0 +1,67 @@
+#define COBJECT_IMPLEMENTATION
+ 
+#include "dbg_log.h"
+
+
+#ifndef CHash_Map_Params
+#error "CHash_Map Params not defined"
+#endif
+
+#define CHash_Map_T TEMPLATE(CHash_Map, CHash_Map_Params)
+#define CHash_Map_Class_T TEMPLATE(CHash_Map, CHash_Map_Params, Class)
+#define CHash_Map_Pair_T TEMPLATE(Pair, CHash_Map_Params, T)
+#define KEY_T T_Param(1, CHash_Map_Params)
+#define OBJ_T T_Param(2, CHash_Map_Params)
+#define CHash_Map_Method(method) TEMPLATE(CHash_Map, CHash_Map_Params, method)
+
+#define CHash_Set_Params CHash_Map_Pair
+#include "chash_set.c"
+typedef CHash_Set_T CHash_Map_T;
+
+static void CHash_Map_Method(delete)(struct Object * const obj);
+static int CHash_Map_Method(cmp)(CHash_Map_Pair_T * a, CHash_Map_Pair_T * b);
+
+CHash_Map_Class_T CHash_Map_Class = 
+{
+    {CHash_Map_Method(delete), NULL},
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+};
+
+static CHash_Map_T CHash_Map = {NULL};
+
+void CHash_Map_Method(delete)(struct Object * const obj)
+{
+}
+
+int CHash_Map_Method(cmp)(CHash_Map_Pair_T * a, CHash_Map_Pair_T * b)
+{
+    return a->key - b->key;
+}
+
+void Method_Name(Populate, CHash_Map, CHash_Map_Params)(CHash_Map_T * const this, CHash_Map_Pair_T * const buff, size_t buff_size,
+                 CHash_Map_Cmp_T cmp)
+{
+    if(NULL == Method_Name(CHash_Map, CHash_Map_Params).vtbl)
+    {
+        Method_Name(Populate, CHash_Set, CHash_Set_Params)(&this->Method_Name(CHash_Set, CHash_Set_Params), buff, buff_size, CHash_Map_Method(cmp));
+        Object_Init(&Member_Name(CHash_Map, CHash_Map_Params).Object, &Member_Name(CHash_Map, CHash_Map_Params, Class).Class,
+                    sizeof(Member_Name(CHash_Map, CHash_Map_Params,Class).CHash_Set));
+    }
+    Method_Name(Populate, CHash_Set, CHash_Set_Params)(&this->Method_Name(CHash_Set, CHash_Set_Params), buff,
+                buff_size, (cmp)? cmp : CHash_Map_Method(cmp));
+    this->vtbl = &Member_Name(CHash_Map, CHash_Map_Params, Class);
+}
+
+CHash_Map_Pair_T CHash_Map_Method(make_pair)(KEY_T const key, OBJ_T const obj)
+{
+    CHash_Map_Pair_T pair = {key, obj};
+    return pair;
+}
---
+++ chash_map.h
@@ -0,0 +1,49 @@
+#if !defined(CHASH_MAP_H_) || defined(CHash_Map_Params)
+#define CHASH_MAP_H_
+
+#ifndef CHash_Map_Params
+#error "CHash_Map_Params not defined"
+#endif
+
+#define CHash_Map_T TEMPLATE(CHash_Map, CHash_Map_Params)
+#define CHash_Map_Class_T TEMPLATE(CHash_Map, CHash_Map_Params, Class)
+#define CHash_Map_Pair_T TEMPLATE(Pair, CHash_Map_Params, T)
+#define KEY_T T_Param(1, CHash_Map_Params)
+#define OBJ_T T_Param(2, CHash_Map_Params)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+typedef struct Member_Name(Pair, CHash_Map_Params)
+{
+    KEY_T key;
+    OBJ_T obj;
+}CHash_Map_Pair_T;
+
+#define CHash_Set_Params CHash_Map_Pair
+#include "chash_set.h"
+
+typedef CHash_Set_T CHash_Map_T;
+typedef CHash_Set_Cmp_T CHash_Map_Cmp_T;
+
+typedef union CHash_Set_Class_T CHash_Map_Class_T;
+
+#undef CHash_Set_Params
+
+extern CHash_Map_Class_T _private CHash_Map_Class;
+
+extern void Method_Name(Populate, CHash_Map, CHash_Map_Params)(CHash_Map_T * const cmap, CHash_Map_Pair_T * const buff, size_t const buff_size, CHash_Map_Cmp_T const compare);
+
+extern CHash_Map_Pair_T CHash_Map_Method(make_pair)(KEY_T const key, OBJ_T const obj);
+
+#ifdef __cplusplus
+}
+#endif
+
+#undef CHash_Map_T 
+#undef CHash_Map_Class_T 
+#undef CHash_Map_Pair_T 
+#undef KEY_T
+#undef OBJ_T
+
+#endif /*CHASH_MAP_H_*/
