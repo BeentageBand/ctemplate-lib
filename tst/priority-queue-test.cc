49833b46cb101a803739d246dc3cc2d2e0636c06
BeentageBand <josera.baez@gmail.com>
2020-09-26 15:51:40 -0700

    Fix CPriorityQueue unit-tests

--- tst/priority-queue-test.cc
+++ tst/priority-queue-test.cc
@@ -26,24 +26,26 @@ static union CPriorityQueue_int CPriorityQueueC = {NULL};
 TEST(CPriorityQueue,functionalities)
 {
 	CPriorityQueue_int_populate(&CPriorityQueueC, NULL);
+    std::make_heap(PQueueCPP.begin(), PQueueCPP.end(), std::greater<int>());
 }
 
 TEST_P(Test_PQueue, insert_n_find)
 {
 	EXPECT_EQ(PQueueCPP.size(), CPriorityQueue_int_size(&CPriorityQueueC));
+	printToCout(PQueueCPP);
 
-  std::make_heap(PQueueCPP.begin(), PQueueCPP.end());
 	PQueueCPP.push_back(GetParam());
+	std::push_heap(PQueueCPP.begin(), PQueueCPP.end(), std::greater<int>());
 
 	CPriorityQueue_int_push(&CPriorityQueueC, GetParam());
 
-	EXPECT_EQ(PQueueCPP.back(), CPriorityQueue_int_peek(&CPriorityQueueC));
+	EXPECT_EQ(PQueueCPP.front(), CPriorityQueue_int_poll(&CPriorityQueueC));
 }
 
 TEST_P(Test_PQueue, destroy)
 {
 	PQueueCPP.pop_back();
-	CPriorityQueue_int_poll(&CPriorityQueueC);
+	CPriorityQueue_int_peek(&CPriorityQueueC);
 
 	EXPECT_EQ(PQueueCPP.size(), CPriorityQueue_int_size(&CPriorityQueueC));
 
